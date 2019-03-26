bff8dbf3dd7a3588fd9e597592e7c7d8e37db66d
onisley <onisley@naver.com>
2019-03-26 21:10:21 +0900

    add AWS Sample Code

--- onisley/testcode/main.js
+++ onisley/testcode/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// On this codelab, you will be streaming only video (video: true).
+
 const mediaStreamConstraints = {
   video: true,
 };
 
-// Video element where stream will be placed.
+// 스트리밍할 비디오를 받아올 변수
 const localVideo = document.querySelector('video');
 
 // Local stream that will be reproduced on the video.
@@ -22,6 +22,10 @@ function handleLocalMediaStreamError(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
-// Initializes media stream.
+// navigator: 브라우저 정보를 포함한 객체
+// mediaDevices.getUserMedia: 사용자에게 미디어 장치 사용 권한을 요청, 요청 수락시 미디어 트랙을 포함한 MediaStream 을 반환
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
   .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+  /*js promise: 자바스크립트 비동기 처리에 사용되는 객체*/
+  // A.then(B): A의 실행이 끝나면 B 실행
+  // A.catch(B): A가 실패 상태가 되면 B에 저장
