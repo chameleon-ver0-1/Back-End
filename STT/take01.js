878e80baba7af2f8e01324753964269981fb7b24
onisley <onisley@naver.com>
2019-03-28 21:43:46 +0900

    add webRTC audio STT code

--- STT/take01.js
+++ STT/take01.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // 회의가 진행 중이면 1, 아니면 0을 가지는 변수
-let onAir = 0;
+let onAir = false;
 
 // 음성인식 관련
 const recognition = new webkitSpeechRecognition();
@@ -9,6 +9,13 @@ const language = 'ko-KR';
 const two_line = /\n\n/g;
 const one_line = /\n/g;
 const first_char = /\S/;
+
+const $btnMic = $('#btn-mic');
+const $result = $('#result');
+
+// 계속 음성인식 & 중간결과를 리턴하도록 option set
+recognition.continuous = true;
+recognition.interimResults = true;
 
 // 미디어 접근 관련
 const mediaStreamConstraints = {
@@ -43,7 +50,7 @@ function onBtnJoin() {
     navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
 
     // 회의 진행 상태로 변경
-    onAir = 1;
+    onAir = true;
 
     console.log("onBtnJoin()");
     console.log("onAir: " + onAir);
@@ -62,8 +69,88 @@ function onBtnExit() {
     alert("Done");
 
     // 회의 종료 상태로 변경
-    onAir = 0;
+    onAir = false;
 
     console.log("onBtnExit()");
     console.log("onAir: " + onAir);
+};
+
+//--------------------------------------------------------
+//-----------------음성인식코오오오오오오드-----------------
+//--------------------------------------------------------
+
+
+
+/**
+ * 음성 인식 시작 처리
+ */
+recognition.onstart = function() {
+  console.log('onstart', arguments);
+  onAir = true;
+  $btnMic.attr('class', 'on');
+};
+
+/**
+ * 음성 인식 종료 처리
+ * @returns {boolean}
+ */
+recognition.onend = function() {
+  console.log('onend', arguments);
+  onAir = false;
+
+  if (ignoreEndProcess) {
+    return false;
+  }
+
+  // DO end process
+  $btnMic.attr('class', 'off');
+  if (!finalTranscript) {
+    console.log('empty finalTranscript');
+    return false;
+  }
+};
+
+/**
+ * 음성 인식 결과 처리
+ * @param event
+ */
+recognition.onresult = function(event) {
+  console.log('onresult', event);
+
+  let interimTranscript = '';
+  if (typeof(event.results) === 'undefined') {
+    recognition.onend = null;
+    recognition.stop();
+    return;
+  }
+
+  for (let i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      finalTranscript += event.results[i][0].transcript;
+    } else {
+      interimTranscript += event.results[i][0].transcript;
+    }
+  }
+
+  finalTranscript = capitalize(finalTranscript);
+  final_span.innerHTML = linebreak(finalTranscript);
+  interim_span.innerHTML = linebreak(interimTranscript);
+
+  console.log('finalTranscript', finalTranscript);
+  console.log('interimTranscript', interimTranscript);
+  fireCommand(interimTranscript);
+};
+
+/**
+ * 음성 인식 에러 처리
+ * @param event
+ */
+recognition.onerror = function(event) {
+  console.log('onerror', event);
+
+  if (event.error.match(/no-speech|audio-capture|not-allowed/)) {
+    ignoreEndProcess = true;
+  }
+
+  $btnMic.attr('class', 'off');
 };
