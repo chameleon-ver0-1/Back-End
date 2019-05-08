4f5e605cfdeb06636b0ec7bb1181526087ad7b00
yeji <yeji9175@naver.com>
2019-05-09 02:20:51 +0900

    ..

--- img_detection/public/js/main.js
+++ img_detection/public/js/main.js
@@ -1,31 +1,82 @@
-'use strict';
+// Global Vars
+let width = 500,
+    height = 0,
+    streaming = false;
 
-// var canvas = document.getElementById(canvas);
-// fs.writeFileSync("image/capture.jpg",canvas);
+// DOM Elements
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const photos = document.getElementById('photos');
+const photoButton = document.getElementById('photo-button');
+const clearButton = document.getElementById('clear-button');
+//예지
+const transImg  = document.getElementById('img');
 
+// Get media stream
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(function(stream) {
+    // Link to the video source
+    video.srcObject = stream;
+    // Play video
+    video.play();
+  })
+  .catch(function(err) {
+    console.log(`Error: ${err}`);
+  });
 
-// On this codelab, you will be streaming only video (video: true).
-const mediaStreamConstraints = {
-  video: true,
-};
+  // Play when ready
+  video.addEventListener('canplay', function(e) {
+    if(!streaming) {
+      // Set video / canvas height
+      height = video.videoHeight / (video.videoWidth / width);
 
-// Video element where stream will be placed.
-const localVideo = document.querySelector('video');
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
 
-// Local stream that will be reproduced on the video.
-let localStream;
+      streaming = true;
+    }
+  }, false);
 
-// Handles success by adding the MediaStream to the video element.
-function gotLocalMediaStream(mediaStream) {
-  localStream = mediaStream;
-  localVideo.srcObject = mediaStream;
-}
+  // Photo button event
+  photoButton.addEventListener('click', function(e) {
+    takePicture();
 
-// Handles error by logging a message to the console with the error message.
-function handleLocalMediaStreamError(error) {
-  console.log('navigator.getUserMedia error: ', error);
-}
+    e.preventDefault();
+  }, false);
 
-// Initializes media stream.
-navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+  // Clear event
+  clearButton.addEventListener('click', function(e) {
+    // Clear photos
+    photos.innerHTML = '';
+  });
+
+  // Take picture from canvas
+  function takePicture() {
+    // Create canvas
+    const context = canvas.getContext('2d');
+    if(width && height) {
+      // set canvas props
+      canvas.width = width;
+      canvas.height = height;
+      // Draw an image of the video on the canvas
+      context.drawImage(video, 0, 0, width, height);
+
+      // Create image from the canvas
+      const imgUrl = canvas.toDataURL('image/png');
+
+      // Create img element
+      const img = document.createElement('img');
+
+      // Set img src
+      img.setAttribute('src', imgUrl);
+
+      //예지
+      transImg.value = imgUrl;
+      document.getElementById("form").submit();
+
+      // Add image to photos
+      photos.appendChild(img);
+    }
+  }
