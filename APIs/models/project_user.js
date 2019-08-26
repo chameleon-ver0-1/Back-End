193dcb4a5628b3f04426a5ea41e49c59b04fd6e8
onisley <onisley@naver.com>
2019-08-26 21:33:09 +0900

    [UPDATE] DB - project_user 테이블에 역할, 관리자 여부 추가

---
+++ APIs/models/project_user.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('project_user', {
+        projectName: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            primaryKey: true
+        },
+        projectRole: {
+            type: DataTypes.STRING(40),
+            allowNull: true
+        },
+        isAdminYn: {
+            type: DataTypes.STRING(1),
+            defaultValue: 'N'
+        }
+    }, {
+        charset: 'utf8',
+        collate: 'utf8_unicode_ci',
+        timestamps: true,
+        paranoid: true
+    });
+}
--- APIs/routes/api/project/index.js
+++ APIs/routes/api/project/index.js
@@ -4,7 +4,6 @@ const controller = require('./project.controller');
 const passport = require('passport');
 const passportConf = require('../common/passport');
 
-const passportSignIn = passport.authenticate('local', {session: false });
 const passportJWT = passport.authenticate('jwt', { session: false });
 
 /* ROUTING METHOD */
