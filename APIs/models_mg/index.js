3e343e193e8ec3fcbb88b9248ea7f9334b5cc0bc
onisley <onisley@naver.com>
2019-08-30 10:18:54 +0900

    [CREATE] MongoDB Issue Schema

--- APIs/models_mg/index.js
+++ APIs/models_mg/index.js
@@ -6,14 +6,15 @@ const db = {};
 
 //TODO: 예지 - MongoDB 연결
 var mongodb = mongoose.connection;
+
 mongodb.on('error', console.error);
 mongodb.once('open', function(){
-    console.log("Connected to mongod server");
+    console.log("Connected to mongodb server");
 });
 mongoose.connect('mongodb://localhost/develop');
 
 db.mongoose = mongoose;
 db.Project = require('./project')(db.mongoose);
-
+db.Issue = require('./issue')(db.mongoose);
 
 module.exports = db;
