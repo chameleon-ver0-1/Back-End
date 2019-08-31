b93e5a2ba2c640b6753b0225448fa417166583be
onisley <onisley@naver.com>
2019-08-31 14:35:58 +0900

    [UPDATE] project create / [CREATE] issue create(task, comment)

--- APIs/routes/api/issue/index.js
+++ APIs/routes/api/issue/index.js
@@ -7,9 +7,12 @@ const passportConf = require('../common/passport');
 const passportJWT = passport.authenticate('jwt', { session: false });
 
 /* ROUTING METHOD */
-router.post('/create', controller.create);
-// router.get('/get', passportJWT, controller.getList);
-router.get('/get/:id', controller.getComments);
-// router.post('/delete/:id', passportJWT, controller.delete);
+router.post('/create/task', controller.createIssue); // FIXME:
+//router.get('/get', controller.getList);
+router.get('/get/:id', controller.getComments); // FIXME:
+//router.post('/delete/:id', passportJWT, controller.deleteTask);
+
+// comment
+router.post('/create/comment', controller.createComment); // FIXME:
 
 module.exports = router;
