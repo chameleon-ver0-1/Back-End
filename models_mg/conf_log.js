1a6174c26a542159c38ed592eed4e90e184e617e
onisley <onisley@naver.com>
2019-11-06 11:14:27 +0900

    Modify detailId to String(not FK)

--- models_mg/conf_log.js
+++ models_mg/conf_log.js
@@ -10,7 +10,7 @@ conf_logs = (mongoose) => {
         totalLogFile : String,  //정제되지 않은(전체 회의기록) txt 파일이 있는 경로
         roomId: String,
         projectId : { type: mongoose.Schema.Types.ObjectId, ref: 'project'}, 
-        detailId : { type: mongoose.Schema.Types.ObjectId, ref: 'conf_log_detail'}
+        detailId : String
     });
     schema.plugin(mongoosePaginate);
     return mongoose.model('conf_log', schema );
